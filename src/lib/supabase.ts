c40a062c2bdcf2721bc5c5cffb080ab8f1c9b169
IsuruAchintha <89300672+AchinthaDavidson@users.noreply.github.com>
2025-07-08 09:34:34 +0530

    0.0.22

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!||"https://agqxhzskevmznwbixoac.supabase.co"
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!||"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFncXhoenNrZXZtem53Yml4b2FjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MDg2NjAyOSwiZXhwIjoyMDY2NDQyMDI5fQ.OqYZ5x34YPPwHJGQ1KJRhqcQWpB0AI-EwVEFC6GQgYs"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
 // Create a Supabase client with service role key for server-side operations
 export const supabase = createClient(supabaseUrl, supabaseServiceKey, {
