dea6fffc58abf881db0c04968d635e491fa5b113
IsuruAchintha <89300672+AchinthaDavidson@users.noreply.github.com>
2025-07-09 09:49:19 +0530

    0.0.45

--- src/pages/api/locum-profile/hello1.ts
+++ src/pages/api/locum-profile/hello1.ts
@@ -1,15 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prisma";
 
-type Data = {
-  name: string;
-};
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     switch (req.method) {
       case "GET":
-        return res.status(200).json({ name: "John Doee " });
+        const profiles = await prisma.locumProfile.findMany({
+          include: {
+            specialties: true,
+          },
+          orderBy: { createdAt: "desc" },
+        });
+        return res.status(200).json(profiles);
     }
   } catch (error: any) {
     return res.status(400).json({
