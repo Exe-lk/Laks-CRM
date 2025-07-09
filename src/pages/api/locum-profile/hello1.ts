472ea9f87273244d34d28df589c634bba8d0d36b
IsuruAchintha <89300672+AchinthaDavidson@users.noreply.github.com>
2025-07-09 12:18:43 +0530

    0.0.48

--- src/pages/api/locum-profile/hello1.ts
+++ src/pages/api/locum-profile/hello1.ts
@@ -8,7 +8,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     switch (req.method) {
       case "GET":
         try {
-          return res.status(200).json({name:"guhgu"});
+           // Get all locum profiles
+        const profiles = await prisma.locumProfile.findMany({
+          include: {
+            specialties: true,
+          },
+          orderBy: { createdAt: "desc" },
+        });
+          return res.status(200).json({
+            message:"data",
+            payload:profiles
+          });
         } catch (error) {
           console.error("Error fetching profiles:", error);
           return res.status(500).json({ error: "Failed to fetch profiles" });
